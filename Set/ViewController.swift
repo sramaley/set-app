bb9c5794b3fbfe3c99b02eeef4ab82ea333ad72d
sramaley <sramaley@berkeley.edu>
2018-02-05 13:40:35 -0800

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Stephen  on 2/5/18.
+//  Copyright © 2018 Stephen . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
